711c51ccb9a20e18aa8060ffa0d5125e3b12761b
Filipe Kertcher <filipekertcher97@gmail.com>
2023-07-04 23:07:48 -0300

    frontend code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Card, CardBody,Container,Flex,HStack,Heading,Image,SimpleGrid,Text, VStack } from '@chakra-ui/react';
 
 type Post = {
   userId: number;
@@ -12,21 +13,19 @@ type Post = {
 
 const PostCard = ({post}: {post: Post}) => {
   return (
-    <div key={post.id} style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      marginTop: 20,
-      
-    }}>
-    <img src={post.image} height={200} width={200} alt={'Post'}/>
-    <span>{ 
-      post.title
-    }</span>
-    <span>{ 
-      post.authorName
-    }</span>
-  </div>
+
+    <Card backgroundColor={'white'} borderRadius={5} padding={8} mt={10} maxWidth={400}>
+      <CardBody>
+        <VStack justifyContent={'flex-start'} alignItems={"flex-start"}>
+          <Image src={post.image} maxHeight={200} style={{width: '100%'}}/>
+          <HStack> <Text as='b'> Author Name: </Text><Text>{post.authorName}</Text></HStack>
+          <VStack justifyContent={'flex-start'} alignItems={"flex-start"} padding={0}>
+          <Text>{post.title}</Text>
+          </VStack>
+        </VStack>
+      </CardBody>
+    </Card>
+    
   )
 }
 function App() {
@@ -46,17 +45,22 @@ function App() {
     fetchData()
   },[])
   return (
-    <div className="App">
-      <div>app</div>
-
-
-      <h2>Posts</h2>
-      <div >
+    <>
+     <Flex backgroundColor={'white'} width={'100%'} height={60} margin={0} justifyContent={'center'} alignItems={'center'}>
+      <Heading as='h2' size='2xl'>
+        Posts
+      </Heading>
+      </Flex>
+      <Flex direction={'column'} justifyContent={'center'} alignItems={'center'} backgroundColor={'#afb3b0'}>
+     
+      <SimpleGrid columns={3} spacing={15} maxW="container.lg" alignSelf={'center'} backgroundColor={'#afb3b0'}>
           {
             posts.map(post => (<PostCard post={post}/>))
           }
-      </div>
-    </div>
+      </SimpleGrid>
+    </Flex>
+      </>
+    
   );
 }
 
