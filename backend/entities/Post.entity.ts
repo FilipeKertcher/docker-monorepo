7a651affe4cd5417e54b6ca19acea0378ac76c76
Filipe Kertcher <filipekertcher97@gmail.com>
2023-11-10 16:07:47 -0300

    feat: added graphql types

--- backend/entities/Post.entity.ts
+++ backend/entities/Post.entity.ts
@@ -7,22 +7,30 @@ import {
   Relation,
 } from 'typeorm';
 import { UserORMEntity } from './User.entity';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 
 @Entity({ name: 'posts' })
+@ObjectType()
+@InputType('PostEntity')
 export class PostORMEntity {
+  @Field(() => Number, { nullable: false })
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String, {})
   @Column()
   title: string;
 
+  @Field(() => String, {})
   @Column()
   body: string;
 
+  @Field(() => String, {})
   @Column()
   image: string;
 
   @ManyToOne(() => UserORMEntity, (user: UserORMEntity) => user.id)
   @JoinColumn({ name: 'user_id' })
+  @Field(() => UserORMEntity, { nullable: true })
   user: Relation<UserORMEntity>;
 }
--- backend/entities/User.entity.ts
+++ backend/entities/User.entity.ts
@@ -6,24 +6,33 @@ import {
   Relation,
 } from 'typeorm';
 import { PostORMEntity } from './Post.entity';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 
+@InputType('UserEntity')
+@ObjectType()
 @Entity({ name: 'users' })
 export class UserORMEntity {
+  @Field(() => String, { nullable: false })
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String, {})
   @Column()
   apiId: number;
 
+  @Field(() => String, {})
   @Column()
   name: string;
 
+  @Field(() => String, {})
   @Column()
   username: string;
 
+  @Field(() => String, {})
   @Column()
   email: string;
 
+  @Field(() => PostORMEntity, { nullable: true })
   @OneToMany(() => PostORMEntity, (post) => post.user)
   posts: Relation<PostORMEntity[]>;
 }
